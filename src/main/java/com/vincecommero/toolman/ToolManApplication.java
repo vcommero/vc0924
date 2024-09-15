f4289d6cab9454de80cc7f1b611f8d4bbf64f01f
Vince Commero <vacommero@gmail.com>
2024-09-15 17:21:49 -0500

    [Feature] Base Spring Shell project

---
+++ src/main/java/com/vincecommero/toolman/ToolManApplication.java
@@ -0,0 +1,13 @@
+package com.vincecommero.toolman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolManApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vincecommero/toolman/ToolManApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vincecommero.toolman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolManApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
